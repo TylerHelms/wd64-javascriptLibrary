977c9373f4f90065255e0af928a8cb909d483790
Tyler Helms <tylerhelms@tylers-air.attlocal.net>
2020-09-21 16:51:11 -0400

    9/21

---
+++ asyncProgramming/api_script.js
@@ -0,0 +1,44 @@
+//console.log('Test');
+
+const baseURL = 'https://api.spacexdata.com/v2/rockets';
+
+const searchForm = document.querySelector('form'); 
+const spaceShips = document.querySelector('ul'); 
+
+//eventListener 
+searchForm.addEventListener('submit', fetchSpace);
+
+
+//FETCH FUNCTION 
+function fetchSpace(e) {
+    e.preventDefault(); // this method allows us to cancel a default action by the browser.
+
+    //console.log('Clicked'); 
+
+    fetch(baseURL) // starts the process of fetching from our resource
+    .then(results => {
+        // console.log(results) // show our response 
+        return results.json()   // This method takes in a reponse (results), reads to comepletion and returns the resukts as a json format
+    })
+    .then(json => {
+        // console.log(json)
+        displayRockets(json)
+    })
+}
+
+
+// DISPLAY FUNCTION 
+function displayRockets(data) {
+    console.log('DisplayRocket: ', data); 
+    
+    let rockets = data.forEach(r => {
+        console.log(r);
+        let rocket = document.createElement('li'); //create an li tag
+        // rocket.innerText = r.name
+        rocket.innerText = `${r.name} is in the country ${r.country}`;
+        spaceShips.appendChild(rocket);
+    });
+}
+
+
+
---
+++ asyncProgramming/async-await.js
@@ -0,0 +1,60 @@
+//! Async/Await 
+
+/*
+    -Allows us to program in acynchronous manner while still allowing code to run in the background.
+    - Makes our site as responsice as possible.
+*/
+
+//? Async Function (syntax)
+/*
+    - allows us to make a normal function asynchronous. Can't use "await"
+    -must use the async keyword at the beginning of the function declaration
+*/
+
+async function myFN() {
+    //await
+}
+
+const myAsyncFn = async () => {
+    //await
+}
+
+const newFN = () => {
+    // await (provides a syntax error because async is not declared)
+}
+
+async function fn() {
+    return 'Hello';
+}
+
+fn().then(console.log);
+
+fn().then(dataFromAsyncFun => {
+    console.log(dataFromAsyncFun);
+})
+
+//? Await 
+// pauses at each await expression 
+
+fetch('https://random.dog/woof.json')
+.then(res => res.json())
+.then(json => console.log(json))
+.catch(err => console.log(err)); 
+
+    // - Chaining .then() together returns a promise (resolve/reject) and is known as a method chaining. 
+
+    // const response = await fetch('https://random.dog/woof.json');
+    // const json = await response.json();
+    // console.log(json);
+
+    const request = async () => {
+        const response = await fetch('await fetch(https://random.dog/woof.json');
+        const json = await response.json();
+        return json;
+    }
+    
+    request().then(json => {
+        console.log(json); 
+    })
+
+    //practice by connecting to an HTML and messinf wirh it 
